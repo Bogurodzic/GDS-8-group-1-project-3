0685d4d28cb5e66c96aa8e68a29dc004e4ccbd0c
kyrie93 <47689921+kyrie93@users.noreply.github.com>
2021-07-01 21:35:46 +0200

    FrameLock

---
+++ Assets/Scripts/FrameLock.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrameLock : MonoBehaviour
+{
+    [SerializeField] int targetFPS = 60;
+
+    private void Awake()
+    {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = targetFPS;
+    }
+
+    private void Update()
+    {
+        if (Application.targetFrameRate != targetFPS)
+        {
+            Application.targetFrameRate = targetFPS;
+        }
+    }
+}
