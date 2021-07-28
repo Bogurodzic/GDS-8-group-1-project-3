388c5f270f65c0803ccbb310e991fc86161f813d
Bartek Fedorowicz <fedorowicz93@gmail.com>
2021-07-28 19:32:30 +0200

    temporary UI

---
+++ Assets/Scripts/HUDController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class HUDController : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _hpText;
+    [SerializeField] private PlayerController _player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        _hpText.text = "Current HP: " + _player.currentHealth;
+    }
+}
