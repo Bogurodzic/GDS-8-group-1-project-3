2f42070d49359d1c77dd1e58bffd2c03d4edac83
Kamil Jarząb <jarzbkamil@gmail.com>
2021-08-12 13:14:52 +0200

    Inventory update

--- Assets/Scripts/Inventory/PlayerInventory.cs
+++ Assets/Scripts/Inventory/PlayerInventory.cs
@@ -1,11 +1,29 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerInventory : GenericSingletonClass<GameManager>
 {
-
+    [SerializeField] private GameObject _playerInventory;
+    [SerializeField] private PlayerInventoryUI _playerInventoryUI;
+    
     private LinkedList<Collectible> _collectibles = new LinkedList<Collectible>();
+
+    private bool _inventoryVisible = false;
+    public void Update()
+    {
+        if (!_inventoryVisible && Input.GetKeyDown(KeyCode.I))
+        {
+            _inventoryVisible = true;
+            _playerInventory.SetActive(true);
+            _playerInventory.GetComponent<PlayerInventoryUI>().ShowPlayerInventoryUI();
+        } else if (_inventoryVisible && Input.GetKeyDown(KeyCode.I))
+        {
+            _inventoryVisible = false;
+            _playerInventory.SetActive(false);
+        }
+    }
 
     public void AddCollectible(Collectible collectible)
     {
---
+++ Assets/Scripts/Inventory/PlayerInventoryUI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventoryUI : MonoBehaviour
+{
+    [SerializeField] private GameObject _inventoryItem; 
+    
+    public void ShowPlayerInventoryUI()
+    {
+        LinkedList<Collectible> collectibles =
+            GameObject.Find("PlayerInventory").GetComponent<PlayerInventory>().GetCollectibles();
+
+        Debug.Log(collectibles.Count);
+        int index = 0;
+        foreach (var collectible in collectibles)
+        {
+            Vector3 itemPosition = new Vector3(transform.position.x, transform.position.y - 50 * index, transform.position.z);
+            Instantiate(_inventoryItem, itemPosition, transform.rotation);
+            index++;
+        }
+    }
+}
