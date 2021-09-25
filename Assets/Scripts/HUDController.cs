1f43cd03fa78192fc9c04b709c9d70cc21fae1eb
Bartek Fedorowicz <fedorowicz93@gmail.com>
2021-09-25 17:23:34 +0200

    HP text string as variable

--- Assets/Scripts/HUDController.cs
+++ Assets/Scripts/HUDController.cs
@@ -5,12 +5,16 @@ using TMPro;
 
 public class HUDController : MonoBehaviour
 {
+    [Header("References")]
     [SerializeField] private TextMeshProUGUI _hpText;
     [SerializeField] private PlayerController _player;
+
+    [Header("Text Strings")]
+    [SerializeField] private string _hpTextString;
 
     // Update is called once per frame
     void Update()
     {
-        _hpText.text = "Current HP: " + _player.currentHealth;
+        _hpText.text = _hpTextString + _player.currentHealth;
     }
 }
