7fbc692401069d92bb79cbe10c0511585fd3bd8c
Bartek Fedorowicz <fedorowicz93@gmail.com>
2021-10-11 21:52:35 +0200

    Start menu

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -1,18 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private NextStage _nextStage;
+    [SerializeField] private Animator _crossfade;
+    [SerializeField] private GameObject _tutorialScreen;
+
+    [SerializeField] private string _startText;
+
+    [SerializeField] private TextMeshProUGUI _fakeLoadingText;
+    [SerializeField] private GameObject _fakeLoadingIcon;
+
+    private bool _canStartGame = false;
+
+    private void Start()
     {
-        
+        _fakeLoadingText.text = "Wczytywanie...";
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (_canStartGame)
+        {
+            if (Input.anyKeyDown)
+            {
+                SceneManager.LoadScene(1);
+            }
+        }
     }
+
+    public void StartFakeLoading()
+    {
+        StartCoroutine(FakeLoadingScreen());
+    }
+
+    private IEnumerator FakeLoadingScreen()
+    {
+        _crossfade.SetTrigger("Start");
+
+        yield return new WaitForSeconds(1f);
+
+        _tutorialScreen.SetActive(true);
+
+        yield return new WaitForSeconds(4f);
+
+        _fakeLoadingIcon.SetActive(false);
+        _fakeLoadingText.text = _startText;
+        _canStartGame = true;
+    }
+
 }
